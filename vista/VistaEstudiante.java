76501b6b19eadfdca04c9512b9c036af49d307ad
Gabriel Perez <gperezm@profesores.uniajc.edu.co>
2025-04-22 20:20:49 -0500

    Agregando la clase vista estudiante

---
+++ vista/VistaEstudiante.java
@@ -0,0 +1,14 @@
+package vista;
+
+/**
+ *
+ * @author g.perezmoreno
+ */
+public class VistaEstudiante {
+
+  public void mostrarDetallesEstudiante(String nombre, int edad) {
+    System.out.println("Detalles del estudiante");
+    System.out.println("Nombre: " + nombre);
+    System.out.println("Edad: " + edad);
+  }
+}
