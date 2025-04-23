e93925f88f99d769813dba14ff2a1bd2da511194
Gabriel Perez <gperezm@profesores.uniajc.edu.co>
2025-04-22 20:10:49 -0500

    Creacion del modelo

---
+++ modelo/Estudiante.java
@@ -0,0 +1,27 @@
+package modelo;
+
+/**
+ *
+ * @author g.perezmoreno
+ */
+public class Estudiante {
+  private String nombre;
+  private int edad;
+
+  public String getNombre() {
+    return nombre;
+  }
+
+  public void setNombre(String nombre) {
+    this.nombre = nombre;
+  }
+
+  public int getEdad() {
+    return edad;
+  }
+
+  public void setEdad(int edad) {
+    this.edad = edad;
+  }
+
+}
